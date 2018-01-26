cb2492160a22fb6adfc02008461cab35c28a2434
sengwon <wjs991@naver.com>
2018-01-26 09:03:45 +0900

    서버 수정

--- server/app.js
+++ server/app.js
@@ -40,9 +40,9 @@ app.use('/api/todo',require('./api/todo'));
 app.use('/api/post', require('./api/post'));
 
 // Angular
-app.use(express.static(path.resolve(__dirname, '../app'))); //1
+app.use(express.static(path.resolve(__dirname, '../dist'))); //1
 app.get('*', function (req, res) { //2
-  var indexFile = path.resolve(__dirname,'../index.html');
+  var indexFile = path.resolve(__dirname,'../dist/index.html');
   res.sendFile(indexFile);
 });
 
