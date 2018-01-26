3b0ae4ac54e56a71deb5293d7faa75ff66076749
sengwon <wjs991@naver.com>
2018-01-26 20:26:26 +0900

    다시

--- server/app.js
+++ server/app.js
@@ -40,7 +40,7 @@ app.use('/api/todo',require('./api/todo'));
 app.use('/api/post', require('./api/post'));
 
 // Angular
-app.use(express.static(path.resolve(__dirname, 'src'))); //1
+app.use(express.static(path.resolve(__dirname, '../src'))); //1
 
 app.get('*', function (req, res) { //2
   var indexFile = path.resolve(__dirname,'../src/index.html');
