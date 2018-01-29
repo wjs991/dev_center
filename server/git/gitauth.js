b2b55fcff80afdc70a275de48e74511c8f18788f
sengwon <wjs991@naver.com>
2018-01-29 21:28:26 +0900

    auth 변경

--- server/git/gitauth.js
+++ server/git/gitauth.js
@@ -11,13 +11,30 @@ const octokit = require('@octokit/rest')({
   debug: true
 });
 var giturl = "https://github.com/login/oauth/authorize";
-
+var ClientOAuth2 = require('client-oauth2');
+var githubAuth = new ClientOAuth2({
+  clientId: 'cc2d7e3046aa9e47749e',
+  clientSecret: 'b70de4195f582d14893003a4e0bd04e212e916de',
+  accessTokenUri: 'https://github.com/login/oauth/access_token',
+  authorizationUri: 'https://github.com/login/oauth/authorize',
+  redirectUri: 'https://tmdtmdtmd.herokuapp.com/gitauth/user',
+  scopes: ['notifications', 'gist']
+})
 router.get("/",function(req,res){
-  console.log("//");
-    requset_api.get(`${giturl}/${client_id}`)
-    .on('res',function(res){
-      console.log(response.body);
-    });
+  var token = githubAuth.createToken('access token', 'optional refresh token', 'optional token type', { data: 'raw user data' });
+  // Set the token TTL.
+  token.expiresIn(1234) // Seconds.
+  token.expiresIn(new Date('2016-11-08')) // Date.
+
+  // Refresh the users credentials and save the new access token and info.
+  token.refresh().then(storeNewToken)
+
+  // Sign a standard HTTP request object, updating the URL with the access token
+  // or adding authorization headers, depending on token type.
+    token.sign({
+      method: 'get',
+      url: 'https://api.github.com/users'
+    }) //=> { method, url, headers, ... }
   }
 );
 
