dcfb1f91dd9f58fab3262ef3ca572fc3f637759b
sengwon <wjs991@naver.com>
2018-01-30 09:49:20 +0900

    변경

--- server/git/config.js
+++ server/git/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'GITHUB_KEY': 'your-github-key-here',
-    'GITHUB_SECRET': 'your-github-secret-here'
+    'GITHUB_KEY': "cc2d7e3046aa9e47749e",
+    'GITHUB_SECRET': '1b1bd1db2db2467b990c7c78601a526048b0acdc'
   }
